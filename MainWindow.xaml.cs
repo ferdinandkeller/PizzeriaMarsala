da1ea87ea153f1b2ba48ea976a52e7e28d001ac5
Ferdinand Keller <ferdinandklr@gmail.com>
2021-01-19 22:02:11 +0100

    UI basique
    l'interface peut mtn changer de taille, mais il n'y a qu'une seule vue pour le moment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,23 +1,79 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace PizzeriaMarsala
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        public static List<string> noms;
+        // delegate qui met à jour les variables locales 
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        // delegate utilié pour effectuer des actions lorsque la fenêtre
+        // change de taille
+        public delegate void OnWindowResized();
+        OnWindowResized WindowResized;
+
+        /*
+         * Fonction principale du WPF
+         */
         public MainWindow()
         {
-            noms = new List<string>();
-            noms.Add("Ferdinand");
-            noms.Add("Albert");
-            noms.Add("Flavie");
-            noms.Add("Valentin");
-            noms.Add("Carla");
-
+            // on initialise les composants WPF
             InitializeComponent();
+            // on définit le data context (pour pouvoir accéder
+            // aux variables de la classe depuis le fichier XAML)
+            this.DataContext = this;
+
+            // on enregistre notre delegate WindowResizeHandler
+            this.SizeChanged += (sender, e) => { WindowResized(); };
+            WindowResized += ResizeWrapPanel;
         }
 
+        /*
+         * Ces deux fonctions font en sorte que le WrapPanel change
+         * de taille en même temps que la fenêtre
+         */
+        private WrapPanel panel = null;
+
+        private int _RectangleWidth = 100;
+        public int RectangleWidth
+        {
+            get => _RectangleWidth;
+            set
+            {
+                _RectangleWidth = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("RectangleWidth"));
+            }
+        }
+
+        private void WrappanelLoaded(object sender, RoutedEventArgs e)
+        {
+            panel = (WrapPanel)sender;
+            ResizeWrapPanel();
+        }
+
+        private void ResizeWrapPanel()
+        {
+            if (panel != null)
+            {
+                int n = (int)panel.ActualWidth / (350 + 2*5);
+                if (n == 0) { n = 1; }
+                RectangleWidth = (int)panel.ActualWidth / n - 2*5;
+            }
+        }
     }
 }
