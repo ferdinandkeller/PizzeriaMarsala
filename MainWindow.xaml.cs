a06554e43034d2726696a0687528fdbb1189cba5
Ferdinand Keller <ferdinandklr@gmail.com>
2021-01-19 14:51:47 +0100

    implémentation bouton coucou

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace PizzeriaMarsala
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Coucou");
+        }
     }
 }
