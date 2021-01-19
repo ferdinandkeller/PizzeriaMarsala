a1b61803ae9e1902a4683f6e3ba9a669e59930cc
Ferdinand Keller <ferdinandklr@gmail.com>
2021-01-19 15:43:03 +0100

    ajout d'une interface graphique basique

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,33 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace PizzeriaMarsala
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static List<string> noms;
+
         public MainWindow()
         {
+            noms = new List<string>();
+            noms.Add("Ferdinand");
+            noms.Add("Albert");
+            noms.Add("Flavie");
+            noms.Add("Valentin");
+            noms.Add("Carla");
+
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Console.WriteLine("Coucou");
-        }
     }
 }
