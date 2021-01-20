785bb9497537130ad432d9ffe904f8373b1f1bc7
Ferdinand Keller <ferdinandklr@gmail.com>
2021-01-20 16:54:43 +0100

    Personne implémente interfaces
    La classe personne implémente maintenant des interfaces utiles

--- Personne.cs
+++ Personne.cs
@@ -6,37 +6,45 @@ using System.Threading.Tasks;
 
 namespace PizzeriaMarsala
 {
-    abstract class Personne
+    /*
+     * Cette classe est abstraite, l'objet Personne n'étant pas utilisé dans l'exercice
+     * On implémente l'interface IEquatable afin de vérifier si deux personnes sont les même
+     * On implémente l'interface IComparable afin de pouvoir trier un tableau de personne par ordre alphabétique
+     */
+    abstract class Personne: IEquatable<Personne>, IComparable<Personne>
     {
+
         protected string Nom { get; private set; }
         protected string Prenom { get; private set; }
         protected string Adresse { get; private set; }
         protected long NumeroTel { get; private set; }
 
-        public Personne()
-        {
-            Nom = null;
-            Prenom = null;
-            Adresse = null;
-            NumeroTel = 0;
-        }
-
-        public Personne(string nom, string prenom, string adresse, long numeroTel)
+        public Personne(string nom, string prenom, string adresse, long numero_tel)
         {
             Nom = nom;
             Prenom = prenom;
             Adresse = adresse;
-            NumeroTel = numeroTel;
+            NumeroTel = numero_tel;
         }
 
         public override string ToString()
         {
-            return this.Nom + " " + this.Prenom + " " + this.Adresse + " " + this.NumeroTel;
+            return $"{Nom} {Prenom} ({NumeroTel}) : habite au {Adresse}";
         }
 
-        public bool Egalite(Personne personne)
+        public bool Equals(Personne personne)
         {
             return Nom == personne.Nom && Prenom == personne.Prenom;
+        }
+
+        public int CompareTo(Personne personne)
+        {
+            int resultat_comparaison = Nom.CompareTo(personne.Nom);
+            if (resultat_comparaison == 0)
+            {
+                resultat_comparaison = Prenom.CompareTo(personne.Prenom);
+            }
+            return resultat_comparaison;
         }
     }
 }
