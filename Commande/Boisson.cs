a1e3356d248ee1b3320b3700d7ee7cafaa9a5c47
Ferdinand Keller <ferdinandklr@gmail.com>
2021-01-20 19:44:47 +0100

    Modifications sur la classe Pizza

--- Commande/Boisson.cs
+++ Commande/Boisson.cs
@@ -9,20 +9,20 @@ namespace PizzeriaMarsala
 {
     class Boisson : Produit
     {
-        public TypeBoisson Nom { get; private set; }
+        public TypeBoisson Type { get; private set; }
         public double Volume { get; private set; } // volume en cl
 
-        public Boisson(TypeBoisson nom_boisson, double volume)
+        public Boisson(TypeBoisson type, double volume)
         {
-            Nom = nom_boisson;
+            Type = type;
             Volume = volume;
 
-            Prix = (double)Nom / 1000 * Volume; // prix en euros
+            Prix = (double)Type / 1000 * Volume; // prix en euros
         }
 
         public override string ToString()
         {
-            return $"{Nom} ({Volume}cl) [{Prix}$]";
+            return $"{Type} ({Volume}cl) [{Prix}$]";
         }
 
     }
---
+++ Commande/TaillePizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzeriaMarsala
+{
+    enum TaillePizza // multiplicateur du prix, en pourcentage
+    {
+        Petite = 80,
+        Moyenne = 100,
+        Grande = 120
+    }
+}
