8cfa519a9417584905823a5f86a8ba99ade34d53
Ferdinand Keller <ferdinandklr@gmail.com>
2021-01-20 16:46:51 +0100

    Correction classe personne
    J'ai simplifié la classe afin de la rendre plus lisible.
    Il y avait aussi une erreur dans le constructeur (pour le numeroTel) que j'ai corrigé

--- Personne.cs
+++ Personne.cs
@@ -8,60 +8,35 @@ namespace PizzeriaMarsala
 {
     abstract class Personne
     {
-        protected string nom;
-        protected string prenom;
-        protected string adresse;
-        protected long numeroTel;
+        protected string Nom { get; private set; }
+        protected string Prenom { get; private set; }
+        protected string Adresse { get; private set; }
+        protected long NumeroTel { get; private set; }
 
         public Personne()
         {
-            this.nom = null;
-            this.prenom = null;
-            this.adresse = null;
-            this.numeroTel = 0;
+            Nom = null;
+            Prenom = null;
+            Adresse = null;
+            NumeroTel = 0;
         }
 
-        public Personne(string nom, string prenom, string adresse, long numero)
+        public Personne(string nom, string prenom, string adresse, long numeroTel)
         {
-            this.nom = nom;
-            this.prenom = prenom;
-            this.adresse = adresse;
-            this.numeroTel = numeroTel;
-        }
-
-        public string Nom
-        {
-            get { return this.nom; }
-        }
-
-        public string Prenom
-        {
-            get { return this.prenom; }
-        }
-
-        public string Adresse
-        {
-            get { return this.adresse; }
-        }
-
-        public long NumeroTel
-        {
-            get { return this.numeroTel; }
+            Nom = nom;
+            Prenom = prenom;
+            Adresse = adresse;
+            NumeroTel = numeroTel;
         }
 
         public override string ToString()
         {
-            return this.nom+" "+this.prenom+" "+this.adresse+" "+this.numeroTel.ToString();
+            return this.Nom + " " + this.Prenom + " " + this.Adresse + " " + this.NumeroTel;
         }
 
-        public bool Egalite(Personne p)
+        public bool Egalite(Personne personne)
         {
-            bool test = false;
-            if(this.nom==p.Nom && this.prenom==p.Prenom && this.adresse == p.Adresse)
-            {
-                test = true;
-            }
-            return test;
+            return Nom == personne.Nom && Prenom == personne.Prenom;
         }
     }
 }
