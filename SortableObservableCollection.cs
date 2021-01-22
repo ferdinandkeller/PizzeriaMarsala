2051f490535219215e0bd42eb23d7682e3799941
Ferdinand Keller <ferdinandklr@gmail.com>
2021-01-22 11:41:13 +0100

    Creation du type de liste SortableObservableCollection

---
+++ SortableObservableCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace PizzeriaMarsala
+{
+    class SortableObservableCollection<T> : ObservableCollection<T>
+    {
+        public delegate int Compare(T el1, T el2);
+
+        public void Sort(Compare func_comparaison)
+        {
+            for (int i = 1; i < this.Count; i++)
+            {
+                T val = this[i];
+                int j = i - 1;
+                while (j >= 0 && func_comparaison(val, this[j]) < 0)
+                {
+                    this[j + 1] = this[j];
+                    j--;
+                }
+                this[j + 1] = val;
+            }
+        }
+    }
+}
